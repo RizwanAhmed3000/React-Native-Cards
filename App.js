75dcfea111a63254836794596263e8292135340a
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-10-20 11:10:22 +0500

    first app

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Card, Text, Image } from 'react-native-elements';
 
+import { data } from './dummyData.js';
+
+const arrayOfData = data.products;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={st.container}>
+      {/* <StatusBar style="auto" /> */}
+      <ScrollView>
+        {arrayOfData.map((item, index) => (
+          <Card key={item?.id} containerStyle={st.card}>
+            <Image source={{ uri: item?.thumbnail }} style={st.image} />
+            <Text style={st.title}>{item?.title}</Text>
+            <Text style={st.description}>{item?.description}</Text>
+            <Text style={st.price}>Price: ${item?.price}</Text>
+          </Card>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const st = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  card: {
+    borderRadius: 10,
+    marginBottom: 10,
+    backgroundColor: "#f7f7f7",
+  },
+  image: {
+    borderTopEndRadius: 10,
+    borderTopLeftRadius: 10,
+    height: 200,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 16,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
